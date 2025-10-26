4508c844cc8b2117934cce4f7aff40e3a26ef693
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-10-26 13:06:42 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const express=require('express');
 const app=express();
 const port=3000;
-const bodyParser=require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const controller=require('./controller/control')
 
+
+
+
+app.use(controller);
+app.use((req,res,next)=>{
+    res.status(404).json({
+        message:"Route not found"
+    });
+})
 app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-})
-
-app.get('/',(req,res)=>{
-   res.sendFile(__dirname+'/router/index.html');
+    console.log(`MVC app listening at http://localhost:${port}`);
 });
-
-app.get('/circle',(req,res)=>{
-   res.sendFile(__dirname+'/router/circle.html');
-});
-
-app.get('/triangle',(req,res)=>{
-   res.sendFile(__dirname+'/router/triangle.html');
-});
-app.post('/circle',(req,res)=>{
-    const redius=req.body.redius;
-    const area=3.1416*(redius*redius);
-    res.send(`<h2>Area of Circle is : ${area}</h2>`);
-})
-app.post('/triangle',(req,res)=>{
-    const base=req.body.base;
-    const height=req.body.height;
-    const area=0.5*base*height;
-    res.send(`<h2>Area of Triangle is : ${area}</h2>`);
-})
