66f58781e0659f4d7a85353f366b0a13d671eff0
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-11-18 23:59:35 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const express=require('express');
-const app=express();
-const port=3000;
-const controller=require('./controller/control')
-
-
-
-
-app.use(controller);
-app.use((req,res,next)=>{
-    res.status(404).json({
-        message:"Route not found"
-    });
-})
-app.listen(port,()=>{
-    console.log(`MVC app listening at http://localhost:${port}`);
+const app = require("./insert");
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
